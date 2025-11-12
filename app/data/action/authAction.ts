f181eb98ff8e78e95bd41ffbf5911d81dd33f3b1
neyzar <lucas.narguet451@gmail.com>
2025-11-12 17:26:18 +0100

    add registerAction

--- app/data/action/authAction.ts
+++ app/data/action/authAction.ts
@@ -1,11 +1,12 @@
 "use server"
+import {z} from "zod"
 
 export async function loginAction(formData : FormData) {
     console.log("bienvenue dans la logique du login")
 
     const fields = {
         email: formData.get('email'),
-        password: formData.get("password")
+        password: formData.get("password"),
     }
 
     console.log(fields)
--- app/data/action/registerAction.ts
+++ app/data/action/registerAction.ts
@@ -1,13 +1,66 @@
 "use server"
+import {z} from "zod"
+import {prisma} from "@/app/lib/prisma";
+import bcrypt from 'bcrypt'
 
-export async function registerAction(formData : FormData) {
+
+const registerSchema = z.object({
+    email: z.string().email('Email invalide'),
+    password: z.string().min(8, 'Mot de passe trop court'),
+    fullName: z.string().min(2, 'Nom requis'),
+
+})
+
+export async function registerAction(formData: FormData) {
     console.log("bienvenue dans la logique du register")
 
-    const fields = {
-        username: formData.get("fullName"),
-        email: formData.get('email'),
-        password: formData.get("password")
-    }
+    try {
+        const data = {
+            fullName: formData.get("fullName"),
+            email: formData.get("email"),
+            password: formData.get("password")
+        }
 
-    console.log(fields)
+        const valideData = registerSchema.parse(data)
+
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: valideData.email
+            }
+        })
+
+        if (existingUser) {
+            return {
+                success: false,
+                message: "user already exists"
+            }
+        }
+
+        const hashedPassword = await bcrypt.hash(valideData.password,10)
+
+        const user = await prisma.user.create({
+            data:{
+                fullName: valideData.fullName,
+                email: valideData.email,
+                password: hashedPassword,
+
+            }
+        })
+
+        return {
+            success: true,
+            user: {
+                id: user.id,
+                email: user.email,
+                fullName: user.fullName
+            }
+        }
+
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return {
+                success: false,
+                error: error.errors[0].message
+            }
+        }}
 }
